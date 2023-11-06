cc86328bac4868eacdf9adff5e9e5ed9bbfa882f
eeseohyun <jinlijinyoung@gmail.com>
2023-11-06 22:42:32 +0900

    Style: 레이아웃 적용

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -1,20 +1,24 @@
+import Layout from '../../../components/layout';
+
 export default function Chats() {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex py-3 px-4 cursor-pointer items-center space-x-3"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jobs</p>
-            <p className="text-sm text-gray-500">
-              내일 퇴근 후, 연락드리겠습니다!
-            </p>
+    <Layout title="채팅" hasTabBar>
+      <div className="py-16 divide-y-[1px]">
+        {[1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex py-3 px-4 cursor-pointer items-center space-x-3"
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jobs</p>
+              <p className="text-sm text-gray-500">
+                내일 퇴근 후, 연락드리겠습니다!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 }
