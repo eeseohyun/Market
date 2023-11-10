0b13bb3aa3d2e27f59e21f7e68897a3e59441261
eeseohyun <jinlijinyoung@gmail.com>
2023-11-11 00:38:54 +0900

    Feat: 로그인 여부 파악 추가

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,13 +5,25 @@ export interface ResponseType {
   [key: string]: any;
 }
 
-export default function withHandler(
-  method: 'GET' | 'POST' | 'DELETE',
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
+interface ConfigProps {
+  method: 'GET' | 'POST' | 'DELETE';
+  fn: (req: NextApiRequest, res: NextApiResponse) => void;
+  isLogin?: boolean; //default: false
+}
+
+export default function withHandler({
+  method,
+  fn,
+  isLogin = true,
+}: ConfigProps) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== method) {
       return res.status(405).end();
+    }
+    if (isLogin && !req.session.user) {
+      return res
+        .status(401)
+        .json({ ok: false, error: '로그인을 진행해주세요.' });
     }
     try {
       await fn(req, res);
