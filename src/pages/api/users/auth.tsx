cf4b1c93ea19f94d07717de9f57d2a26b84abf47
eeseohyun <jinlijinyoung@gmail.com>
2023-11-10 19:19:49 +0900

    Style: type 변경 및 주석처리

--- src/pages/api/users/auth.tsx
+++ src/pages/api/users/auth.tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { client } from '../../../../libs/server/client';
 import withHandler, { ResponseType } from '../../../../libs/server/withHandler';
 import Twilio from 'twilio';
+import smtpTransport from '../../../../libs/server/email';
 
 const twilio = Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -10,7 +11,7 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const user = phone ? { phone: phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
   const randomNum = String(Math.random()).substring(2, 8);
   const token = await client.token.create({
@@ -30,12 +31,34 @@ async function handler(
     },
   });
   if (phone) {
-    await twilio.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `인증 코드 : ${randomNum}`,
-      from: process.env.FROM_PHONE,
-    });
+    // await twilio.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `인증 코드 : ${randomNum}`,
+    //   from: process.env.FROM_PHONE,
+    // });
+  }
+  if (email) {
+    // const mailOptions = {
+    //   from: process.env.MAIL_ID,
+    //   to: email,
+    //   subject: 'MARKET 인증 메일',
+    //   text: `인증 코드 : ${randomNum}`,
+    // };
+    // const result = await smtpTransport.sendMail(
+    //   mailOptions,
+    //   (error, responses) => {
+    //     if (error) {
+    //       console.log(error);
+    //       return null;
+    //     } else {
+    //       console.log(responses);
+    //       return null;
+    //     }
+    //   }
+    // );
+    // smtpTransport.close();
+    // console.log(result);
   }
 
   return res.json({
