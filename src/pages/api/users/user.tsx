4422bde7bec26bfc91623e1a44477b80438a91ea
eeseohyun <jinlijinyoung@gmail.com>
2023-11-11 00:39:58 +0900

    Style: 코드 수정

--- src/pages/api/users/user.tsx
+++ src/pages/api/users/user.tsx
@@ -3,23 +3,14 @@ import { client } from '../../../../libs/server/client';
 import withHandler, { ResponseType } from '../../../../libs/server/withHandler';
 import { withApiSession } from '../../../../libs/server/withSession';
 
-declare module 'iron-session' {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  console.log(req.session);
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
   res.json({ ok: true, profile });
 }
 
-export default withApiSession(withHandler('GET', handler));
+export default withApiSession(withHandler({ method: 'GET', fn: handler }));
