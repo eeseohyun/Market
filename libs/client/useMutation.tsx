1f1af9de8e5b0f8aa11cd284632b6e690b1d02ea
eeseohyun <jinlijinyoung@gmail.com>
2023-11-10 19:19:13 +0900

    Style: type 설정

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 
-export default function useMutation(
+interface UserMutationState<T> {
+  loading: boolean;
+  data?: T;
+  error?: object;
+}
+
+type UserMutationResult<T> = [(data: any) => void, UserMutationState<T>];
+
+export default function useMutation<T = any>(
   url: string
-): [
-  (data: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
+): UserMutationResult<T> {
+  const [state, setState] = useState<UserMutationState<T>>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
   function mutation(data: any) {
-    setLoading(true);
+    setState((prev) => ({ ...prev, loading: true }));
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
@@ -19,9 +26,9 @@ export default function useMutation(
       },
     })
       .then((res) => res.json())
-      .then((json) => setData(json))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
   }
-  return [mutation, { loading, data, error }];
+  return [mutation, { ...state }];
 }
