f09a99d702e27848d49c6a9149102cf1c0e2f825
eeseohyun <jinlijinyoung@gmail.com>
2023-11-12 21:12:20 +0900

    Feat: items api 생성

---
+++ src/pages/api/items/index.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { client } from '../../../../libs/server/client';
+import withHandler, { ResponseType } from '../../../../libs/server/withHandler';
+import { withApiSession } from '../../../../libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === 'GET') {
+    const items = await client.items.findMany({});
+    res.json({ ok: true, items });
+  }
+  if (req.method === 'POST') {
+    const {
+      body: { name, price, description },
+      session: { user },
+    } = req;
+    const item = await client.items.create({
+      data: {
+        name,
+        price: +price,
+        description,
+        image: '',
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, item });
+  }
+}
+
+export default withApiSession(
+  withHandler({ methods: ['GET', 'POST'], fn: handler })
+);
