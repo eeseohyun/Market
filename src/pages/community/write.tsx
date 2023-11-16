953a24345a6b0eb4b7b70b665e866c6219bc8237
eeseohyun <jinlijinyoung@gmail.com>
2023-11-16 21:21:04 +0900

    Feat: 질문 남기기 기능 구현

--- src/pages/community/write.tsx
+++ src/pages/community/write.tsx
@@ -1,13 +1,43 @@
+import { useForm } from 'react-hook-form';
 import Button from '../../../components/button';
 import Layout from '../../../components/layout';
 import TextArea from '../../../components/textArea';
+import useMutation from '../../../libs/client/useMutation';
+import { useEffect } from 'react';
+import { Post } from '@prisma/client';
+import { useRouter } from 'next/router';
+
+interface WriteForm {
+  question: string;
+}
+
+interface WirteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 export default function Write() {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WirteResponse>('/api/posts');
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`community/${data.post.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout title="질문 작성" canGoBack>
-      <form className="px-4 py-16 space-y-4">
-        <TextArea required placeholder="질문을 남겨주세요." />
-        <Button text="완료" />
+      <form onSubmit={handleSubmit(onValid)} className="px-4 py-16 space-y-4">
+        <TextArea
+          register={register('question', { required: true, minLength: 5 })}
+          required
+          placeholder="질문을 남겨주세요."
+        />
+        <Button text={loading ? 'loading...' : '완료'} />
       </form>
     </Layout>
   );
