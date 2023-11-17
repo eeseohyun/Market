7cd95857faf70cbffd4db95bd7583a05d5cf1499
eeseohyun <jinlijinyoung@gmail.com>
2023-11-18 03:30:03 +0900

    Feat: community 페이지기능구현

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,22 +1,42 @@
+import { Post, User } from '@prisma/client';
 import FloatButton from '../../../components/floatButton';
 import Layout from '../../../components/layout';
+import useSWR from 'swr';
+import Link from 'next/link';
 
+interface PostsWithUser extends Post {
+  user: User;
+  _count: {
+    wonder: number;
+    answers: number;
+  };
+}
+interface PostsResponse {
+  ok: boolean;
+  posts: PostsWithUser[];
+}
 export default function Community() {
+  const { data } = useSWR<PostsResponse>(`/api/posts`);
+  console.log(data);
   return (
     <Layout title="동네생활" hasTabBar>
       <div className="py-16 px-4 space-y-8">
-        {[1, 2, 3, 4, 5, 6].map((_, i) => (
-          <div key={i} className="flex flex-col items-start cursor-pointer ">
+        {data?.posts?.map((post) => (
+          <Link
+            key={post.id}
+            href={`/community/${post.id}`}
+            className="flex flex-col items-start cursor-pointer "
+          >
             <span className="flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
               동네 질문
             </span>
             <div className="mt-2 text-gray-700">
-              <span className="text-orange-500 font-medium">Q.</span> 마라탕
-              맛집은 어딘가요?
+              <span className="text-orange-500 font-medium">Q.</span>{' '}
+              {post.question}
             </div>
             <div className="mt-5 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
-              <span>헬퍼</span>
-              <span>18시간 전</span>
+              <span>{post.user.name}</span>
+              <span>{post.createdAt + ''}</span>
             </div>
             <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
               <span className="flex space-x-2 items-center text-sm">
@@ -34,7 +54,7 @@ export default function Community() {
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
-                <span>궁금해요 1</span>
+                <span>궁금해요 {post._count.wonder}</span>
               </span>
               <span className="flex space-x-2 items-center text-sm">
                 <svg
@@ -51,10 +71,10 @@ export default function Community() {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                <span>답변 1</span>
+                <span>답변 {post._count.answers}</span>
               </span>
             </div>
-          </div>
+          </Link>
         ))}
 
         <FloatButton href="/community/write">
