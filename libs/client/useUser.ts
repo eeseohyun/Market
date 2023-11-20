3d71ea8d5ca9637117a9153309fb2b3c4169dfb0
eeseohyun <jinlijinyoung@gmail.com>
2023-11-20 22:50:25 +0900

    Refactor: type 지정

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,10 +1,16 @@
+import { User } from '@prisma/client';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import useSWR from 'swr';
 
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
+
 export default function useUser() {
   const router = useRouter();
-  const { data, error, isLoading } = useSWR('/api/users/user');
+  const { data, error, isLoading } = useSWR<ProfileResponse>('/api/users/user');
   useEffect(() => {
     if (data && !data.ok) {
       router.replace('/enter');
@@ -12,10 +18,3 @@ export default function useUser() {
   }, [data, router]);
   return { user: data?.profile, isLoading: !data && !error };
 }
-
-//super_cache = {
-//   '/api/users/user' : {
-//     ok:true,
-//      profile:{id: 1, phone: 12345, email:null, name:'유저', ...}
-//   }
-// }
