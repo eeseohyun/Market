a613f56f57ac68affd2aa21d705e5d0681cc6266
eeseohyun <jinlijinyoung@gmail.com>
2023-11-21 00:59:53 +0900

    Refactor: 페이지 제한

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SWRProvider } from '@/app/SWRProvider';
+import useUser from '../../libs/client/useUser';
+
+function CustomUser() {
+  const { user } = useUser();
+  return null;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRProvider>
       <div className="w-full max-w-xl mx-auto">
+        <CustomUser />
         <Component {...pageProps} />
       </div>
     </SWRProvider>
