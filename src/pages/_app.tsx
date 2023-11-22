00c9368c05b9c7bc432f1dd6612ccfdb610a5f52
eeseohyun <jinlijinyoung@gmail.com>
2023-11-22 23:44:58 +0900

    Style: 코드 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SWRProvider } from '@/app/SWRProvider';
 import useUser from '../../libs/client/useUser';
