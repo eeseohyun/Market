98efaa3cc5f9ae868de4c2a19fa143afbb9e674b
eeseohyun <jinlijinyoung@gmail.com>
2023-11-22 23:41:46 +0900

    Bug: style 에러 수정

--- components/button.tsx
+++ components/button.tsx
@@ -5,23 +5,25 @@ interface ButtonProps {
   loading?: boolean;
 }
 
-export default function Button({
+const Button = ({
   large = false,
   onClick,
   text,
   loading = false,
   ...rest
-}: ButtonProps) {
+}: ButtonProps) => {
   return (
     <button
       {...rest}
       disabled={loading}
-      className={`w-full bg-orange-400 hover:bg-orange-500 text-white px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none
+      className={`bg-orange-400 hover:bg-orange-600 w-full text-white px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none
         ${large ? 'py-3 text-base' : 'py-2 text-sm'} ${
         loading ? 'opacity-50 cursor-not-allowed' : ''
       }`}
     >
-      {text}
+      <span>{text}</span>
     </button>
   );
-}
+};
+
+export default Button;
